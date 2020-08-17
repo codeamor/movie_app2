2f7e2f6d686b78bedd130b95dc6915a26689a1fc
codeamor <ssm2319@gmail.com>
2020-08-17 20:43:30 +0900

    Updates

---
+++ precache-manifest.bf8436164a2a2c81822cccb0a01bf165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301814c85743fe304b6361aff3508895",
+    "url": "/github.io/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e0fbbdaa0f21ea4267f7",
+    "url": "/github.io/movie_app_2019/static/css/main.bda6f60c.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/github.io/movie_app_2019/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/movie_app_2019/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0fbbdaa0f21ea4267f7",
+    "url": "/github.io/movie_app_2019/static/js/main.5082c006.chunk.js"
+  },
+  {
+    "revision": "6765d52425b26c7aba78",
+    "url": "/github.io/movie_app_2019/static/js/runtime-main.e98dbf79.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/codeamor/github.io/movie_app_2019/precache-manifest.7eb9eed732f978a418bdd04cc89c9d89.js"
+  "/github.io/movie_app_2019/precache-manifest.bf8436164a2a2c81822cccb0a01bf165.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/codeamor/github.io/movie_app_2019/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/movie_app_2019/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
